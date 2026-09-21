2bf237476931cf86a6e125d58330440c8149e45d
Hesborn Anyandarua <anyandarhesbon@gmail.com>
2026-09-21 12:26:38 +0300

    Send Bulk Emails tool implementaion

--- awd_main/settings.py
+++ awd_main/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     
     'dataentry',
     'uploads',
+    'emails',
 ]
 
 MIDDLEWARE = [
--- awd_main/urls.py
+++ awd_main/urls.py
@@ -14,5 +14,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    # emails
+    path('emails/', include('emails.urls')),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
